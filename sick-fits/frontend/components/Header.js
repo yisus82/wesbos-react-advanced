3fab1f96c2774a0facaae300e382ee23f4195554
yisus82 <djalma8@gmail.com>
2021-02-09 10:15:41 +0100

    Global styles, typography & layout styles

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 import Nav from './Nav';
 
 const Logo = styled.h1`
-  background-color: red;
+  background-color: var(--red, red);
   font-size: 4rem;
   margin-left: 2rem;
   position: relative;
   z-index: 2;
   transform: skew(-7deg);
   a {
-    color: white;
+    color: var(--white, white);
     text-decoration: none;
     text-transform: uppercase;
     padding: 0.5rem 1rem;
