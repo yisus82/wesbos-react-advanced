12fcca9c0c2a3b4e8f8790c85b4c8b4d51d74282
yisus82 <djalma8@gmail.com>
2021-02-09 13:00:35 +0100

    Server styles + favicon

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,9 +1,21 @@
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class Document extends NextDocument {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render = () => (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
